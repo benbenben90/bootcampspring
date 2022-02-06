e62d931b252e49cb15f5f4eb478f5f05437df8ca
benbenben90 <med.benmrad90@gmail.com>
2022-02-06 16:19:24 +0100

    deuxieme partie

--- src/main/java/com/sip/ams/controllers/HomeController.java
+++ src/main/java/com/sip/ams/controllers/HomeController.java
@@ -1,13 +1,22 @@
 package com.sip.ams.controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
 	@RequestMapping("/info")
-	public String info() {
+	public String info(Model model) {
+		String formation ="FullStack";
+		List<String> names = new ArrayList<String>(
+			      Arrays.asList("OCP", "OCP", "Spring", "Angular"));
+		model.addAttribute("names", names);
 		return "home/info"; // le nom de la vue sous template /home
 	}
 	
